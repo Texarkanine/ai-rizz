1758d2bbfbb0797750b81f6b6f38437b77461db7
Texarkanine <texarkanine@protonmail.com>
2025-05-29 21:14:51 -0500

    feat: ready to pr!

--- temp_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-. "./tests/common.sh"
-source_ai_rizz
-cmd_init "test_repo" -d "test_target" --local
-cmd_add_rule "rule1.mdc" --commit
-echo "About to run deinit test..."
-output=$(echo "" | cmd_deinit 2>&1 || echo "ERROR_OCCURRED")
-echo "After deinit test, output: $output"
--- test_init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-. "./tests/common.sh"
-source_ai_rizz
-cmd_init "test_repo" -d "test_target" --local
